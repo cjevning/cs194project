fbe46dfd61d23e32679fd76caef4dbbe70f058db
Travis Sanchez <jtsanch@stanford.edu>
2014-02-09 17:52:51 -0800

    deleting files

--- EventApp/app/models/facebook_oauth_setting.rb
+++
@@ -1,3 +0,0 @@
-class FacebookOauthSetting < ActiveRecord::Base
-	belongs_to :user
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-MyEventApp::Application.config.session_store :cookie_store, key: '_MyEventApp_session'
