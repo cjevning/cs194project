4d2612e395a37679e33aa5b3536b76091377363d
Travis Sanchez <jtsanch@stanford.edu>
2014-02-19 22:19:01 -0800

    Fixed invitation bug

--- MyEventApp/app/controllers/home_controller.rb
+++ MyEventApp/app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 	before_filter :authenticate_user!
 	def index
-        @invitations = Invitations.where( user_id: current_user.id )
-        @events = Event.where( user_id: current_user.id )
+        @invitations = Invitations.where( user: current_user )
+        @events = Event.where( user: current_user )
         @events.each do |event|
             puts event.name
             puts event.user_id
