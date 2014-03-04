2941e5045283d940f0834db0d0971b899137229f
Matt Bettonville <mattbettonville@rescomp-13-262652.stanford.edu>
2014-03-04 09:39:25 -0800

    Calendar animated and linked to events

--- MyEventApp/app/assets/javascripts/calendar.js
+++ MyEventApp/app/assets/javascripts/calendar.js
@@ -3,6 +3,20 @@ var eventActive = null;
 var touchOffsetX = 0;
 var touchOffsetY = 0;
 var timeSlots = new Array();
+var activeHour = null;
+var lastViewed = null;
+
+// Some swipe detection code from http://www.javascriptkit.com/javatutors/touchevents2.shtml
+var startX,
+	startY,
+	swipeDir,
+	distX,
+	distY,
+	startTime,
+	threshold = 150,
+	restraint = 100,
+	allowedTime = 300,
+	elapsedTime
 
 
 function loadEvents() {
@@ -25,6 +39,7 @@ function sendEventRequest() {
 			return;
 		} else {
 			var eventArray = JSON.parse(eventXhr.responseText);
+			console.log(eventArray);
 			for (var event in eventArray) {
 				var eventCopy = eventArray[event];
 				processEvent(eventCopy);
@@ -38,14 +53,14 @@ function sendEventRequest() {
 function processEvent(event) {
 	dateSeconds = event["start_in_seconds"];
 	date = new Date(Number(dateSeconds)*1000);
-	if (timeSlots[date.getHours()] != null) {
-		return;
-	}
-	var hourContainer = document.getElementById('hour' + date.getHours());
+	//if (timeSlots[date.getHours()] != null) {
+	//	return;
+	//}
+	var hourContainer = document.getElementById('hour' + date.getUTCHours());
 
 	eventElement = createEventElement(event);
 	hourContainer.appendChild(eventElement);
-	addListeners(eventElement);
+	addCalTouchListeners(eventElement);
 	timeSlots[date.getHours()] = eventElement;
 }
 
@@ -72,63 +87,289 @@ function createEventElement(event) {
 
 
 
-function addListeners(item) {
-		
-	item.addEventListener("touchstart", function(e) {
-		if (eventActive != null) {
-			return;
-		}
-		e.preventDefault();
-		var x = e.changedTouches[0].pageX;
-		var y = e.changedTouches[0].pageY;
-		eventActive = item;
-		
-		//Finger offsets from top left corner of element
-		touchOffsetX = x - item.getBoundingClientRect().left;
-		touchOffsetY = y - item.getBoundingClientRect().top;
-		
-	});
-	
-	item.addEventListener("touchmove", function(e) {
-		if (eventActive == null || eventActive != item) {
-			return;
-		}
-		//e.preventDefault();
-		var x = e.changedTouches[0].pageX;
-		var y = e.changedTouches[0].pageY;
-		adjustedX = x - touchOffsetX;
-		adjustedY = y - touchOffsetY + window.pageYOffset; //Add vertical scroll offset
-		
-		item.style.top = adjustedY + "px";
-		item.style.left = adjustedX + "px";
-		//item.setAttribute("style", "top: " + adjustedY + "px; left: " + adjustedX + "px;");
-	});
-	
-	
-	item.addEventListener("touchend", function(e) {
-		item.style.top = null;
-		item.style.left = null;
-		//item.style.position = "static";
-		eventActive = null;
-		
-		//e.preventDefault();
-		//var orig = e.originalEvent;
-		//var x = orig.changedTouches[0].pageX;
-		//var y = orig.changedTouches[0].pageY;		
+function eventClicked(e) {
+	alert("Clicked");
+}
 
-	});
-	
-	
-	item.addEventListener("touchcancel", function(e) {
-		item.style.position = "static";
-		eventActive = null;
-		item.setAttribute("style", "top: 0px; left: 0px;");
 
-		//e.preventDefault();
-		//var orig = e.originalEvent;
-		//var x = orig.changedTouches[0].pageX;
-		//var y = orig.changedTouches[0].pageY;
-				
-	});
+
+
+function eventAccepted(item) {
+	item.parentElement.removeChild(item);
+	alert("Accepted!");
+}
+
+
+
+function eventRejected(item) {
+	item.parentElement.removeChild(item);
+	alert("Rejected!");
+}
+
+
+
+
+
+function getSmoothedValue(fraction) {
+	if (fraction >= .83666) return .7;
+	else return Math.pow(fraction, 2);
+}
+
+
+
+function adjustOverlay(x) {
+	// Retrieve or create overlay
+	var overlay = document.getElementById('overlay');
+	if (overlay == null) {
+		overlay = document.createElement("DIV");
+		overlay.setAttribute("id","overlay");
+		overlay.style.top = window.pageYOffset + "px";
+		document.body.appendChild(overlay);
+	}
 	
+	// Compute element relative location (fraction moved to either side)
+	var xFraction;
+	if (x > startX) {
+		xFraction = (x - startX) / (document.body.clientWidth - startX);
+	} else {
+		xFraction = (x - startX) / startX;
+	}
+	//xFraction = (x - startX) / (document.body.clientWidth - startX);
+	
+	// Adjust opacity
+	if (xFraction > 0) {
+		overlay.setAttribute("class", "overlay_neg");
+		//overlay.style.backgroundImage = "-webkit-linear-gradient(left,rgba(184,0,0,0),rgba(184,0,0," + getSmoothedValue(Math.abs(xFraction)) + "))";
+		overlay.style.opacity = getSmoothedValue( Math.abs(xFraction) );
+	}
+	if (xFraction < 0) {
+		overlay.setAttribute("class", "overlay_pos");
+		//overlay.style.backgroundImage = "-webkit-linear-gradient(right,rgba(0,184,0,0),rgba(0,184,0," + getSmoothedValue(Math.abs(xFraction)) + "))";
+		overlay.style.opacity = getSmoothedValue( Math.abs(xFraction) );
+	}
+}
+
+
+
+function handleswipe(item, dir) {
+	if (dir == 'right') {
+		eventRejected(item);
+		return 1;
+	}
+	if (dir == 'left') {
+		eventAccepted(item);
+		return 2;
+	}
+	if (dir == 'up') {
+		alert('up swipe');
+		return null;
+	}
+	if (dir == 'down') {
+		alert('down swipe');
+		return null;
+	}
+}
+
+
+
+function calClick(e) {
+	item = e.target;
+	console.log(item);
+	console.log(item.className);
+	if (item.className != "panel panel-info event_item") item = item.parentElement;
+	item.setAttribute("style", "z-index: 1031;");
+	item.style.left = item.getBoundingClientRect().left + "px";
+	item.style.top = (item.getBoundingClientRect().top + window.pageYOffset) + "px";
+	
+	var $elem = $(item);
+	$elem.animate({top:window.pageYOffset + "px", left:"0px", maxWidth:"100%", width:"100%", height:"118%"});
+	
+	var $header = $(item.firstElementChild);
+	$header.animate({fontSize:"24px"});
+	
+	var $description = $(item.children[1]);
+	$description.animate({fontSize:"16px"});
+	
+	removeCalTouchListeners(item);
+	addEventTouchListeners(item);
+	
+	lastViewed = item;
+}
+
+
+
+function calTouchStart(e) {
+	item = e.target;
+	if (item.className != "panel panel-info event_item") item = item.parentElement;
+	if (eventActive != null) {
+		return;
+	}
+	var x = e.changedTouches[0].pageX;
+	var y = e.changedTouches[0].pageY;
+	eventActive = item;
+
+	//Finger offsets from top left corner of element
+	touchOffsetX = x - item.getBoundingClientRect().left;
+	touchOffsetY = y - item.getBoundingClientRect().top;
+
+	// Swipe detection
+	swipeDir = 'none';
+	startX = x;
+	startY = y;
+	dist = 0;
+	startTime = new Date().getTime();
+	//e.preventDefault();
+}
+
+
+
+function calTouchMove(e) {
+	item = e.target;
+	if (item.className != "panel panel-info event_item") item = item.parentElement;
+	if (eventActive == null || eventActive != item) {
+		return;
+	}
+	var x = e.changedTouches[0].pageX;
+	var y = e.changedTouches[0].pageY;
+	adjustedX = x - touchOffsetX;
+	adjustedY = y - touchOffsetY + window.pageYOffset; //Add vertical scroll offset
+
+	item.style.top = adjustedY + "px";
+	item.style.left = adjustedX + "px";
+	//item.setAttribute("style", "top: " + adjustedY + "px; left: " + adjustedX + "px;");
+
+	adjustOverlay(x);
+	e.preventDefault();
+}
+
+
+
+function calTouchEnd(e) {
+	item = e.target;
+	if (item.className != "panel panel-info event_item") item = item.parentElement;
+
+	//e.preventDefault();
+	var x = e.changedTouches[0].pageX;
+	var y = e.changedTouches[0].pageY;
+	
+	distX = x - startX;
+	distY = y - startY;
+	elapsedTime = new Date().getTime() - startTime;
+	if (elapsedTime <= allowedTime) {  // first condition for swipe met
+		if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+			swipeDir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+		}
+		else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+			swipeDir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+		}
+	}
+	
+	eventActive = null;
+	document.body.removeChild(document.getElementById("overlay"));
+	
+	if (handleswipe(item, swipeDir) != null) {
+		return;
+	}
+	
+	if (x < 20) {
+		eventAccepted(item);
+		return;
+	}
+	
+	if (x > document.body.clientWidth - 20) {
+		eventRejected(item);
+		return;
+	}
+	
+	item.style.top = null;
+	item.style.left = null;
+}
+
+
+
+function calTouchCancel(e) {
+	item = e.target;
+	if (item.className != "panel panel-info event_item") item = item.parentElement;
+	//item.style.position = "static";
+	eventActive = null;
+	item.setAttribute("style", "top: 0px; left: 0px;");
+
+	//e.preventDefault();
+	//var orig = e.originalEvent;
+	//var x = orig.changedTouches[0].pageX;
+	//var y = orig.changedTouches[0].pageY;
+}
+
+
+function addCalTouchListeners(item) {
+	item.addEventListener('click', calClick);
+	item.addEventListener('touchstart', calTouchStart);
+	item.addEventListener('touchmove', calTouchMove);
+	item.addEventListener('touchend', calTouchEnd);
+	item.addEventListener('touchcancel', calTouchCancel);
+}
+
+
+function removeCalTouchListeners(item) {
+	item.removeEventListener('click', calClick);
+	item.removeEventListener('touchstart', calTouchStart);
+	item.removeEventListener('touchmove', calTouchMove);
+	item.removeEventListener('touchend', calTouchEnd);
+	item.removeEventListener('touchcancel', calTouchCancel);
+}
+
+
+
+function restoreLastViewed() {
+	lastViewed.setAttribute("style", "z-index: 11;");
+}
+
+
+
+
+
+function eventClick(e) {
+	var item = e.target;
+	console.log(item);
+	console.log(item.className);
+	if (item.className != "panel panel-info event_item") item = item.parentElement;
+	//item.style.left = item.getBoundingClientRect().left + "px";
+	//item.style.top = (item.getBoundingClientRect().top + window.pageYOffset) + "px";
+	
+	var $elem = $(item);
+	$elem.animate({top:(item.parentElement.getBoundingClientRect().top + window.pageYOffset) + "px",
+			left:item.parentElement.getBoundingClientRect().left + "px",
+			maxWidth:"82%",
+			height:"70px"}, "fast", "linear");
+	
+	var $header = $(item.firstElementChild);
+	$header.animate({fontSize:"14px"}, "fast", "linear");
+	
+	var $description = $(item.children[1]);
+	$description.animate({fontSize:"14px"}, "fast", "linear", restoreLastViewed);
+		
+	removeEventTouchListeners(item);
+	addCalTouchListeners(item);
+}
+
+
+
+
+
+
+function addEventTouchListeners(item) {
+	item.addEventListener('click', eventClick);
+	//item.addEventListener('touchstart', eventTouchStart);
+	//item.addEventListener('touchmove', eventTouchMove);
+	//item.addEventListener('touchend', eventTouchEnd);
+	//item.addEventListener('touchcancel', eventTouchCancel);
+}
+
+
+function removeEventTouchListeners(item) {
+	item.removeEventListener('click', eventClick);
+	//item.removeEventListener('touchstart', eventTouchStart);
+	//item.removeEventListener('touchmove', eventTouchMove);
+	//item.removeEventListener('touchend', eventTouchEnd);
+	//item.removeEventListener('touchcancel', eventTouchCancel);
 }
--- MyEventApp/app/controllers/calendar_controller.rb
+++ MyEventApp/app/controllers/calendar_controller.rb
@@ -6,30 +6,15 @@ class CalendarController < ApplicationController
     
     def events
         @eventList = Array.new
-        oneA = Hash.new
-        oneA["name"] = "Late night coding sesh"
-        oneA["start"] = DateTime.new(2014,1,2,9)
-        oneA["end"] = DateTime.new(2014,1,2,10)
-        oneA["description"] = "RoR and JS all day and all night long all week"
-        oneA["start_in_seconds"] = oneA["start"].to_i
-        @eventList.push(oneA)
-        
-        nineA = Hash.new
-        nineA["name"] = "Early Coffee"
-        nineA["start"] = DateTime.new(2014,1,2,17)
-        nineA["end"] = DateTime.new(2014,1,2,18)
-        nineA["description"] = "Grab a coffee with someone new!"
-        nineA["start_in_seconds"] = nineA["start"].to_i
-        @eventList.push(nineA)
-        
-        noon = Hash.new
-        noon["name"] = "Lunch"
-        noon["start"] = DateTime.new(2014,1,2,20)
-        noon["end"] = DateTime.new(2014,1,2,21)
-        noon["description"] = "Looking for a good chat over lunch"
-        noon["start_in_seconds"] = noon["start"].to_i
-        @eventList.push(noon)
-        puts @eventList
+        current_user.events.each do |event|
+           eventHash = Hash.new
+           eventHash["name"] = event.name
+           eventHash["start"] = event.start
+           eventHash["end"] = event.end
+           eventHash["description"] = event.description
+           eventHash["start_in_seconds"] = event.start.to_i
+           @eventList.push(eventHash)
+        end
 
         render json: @eventList
     end
