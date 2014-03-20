da160c12afdb0cff66759d9cb9dc0abb9c0e08aa
Conner Jevning <cjevning@stanford.edu>
2014-03-19 20:05:29 -0700

    redo

--- MyEventApp/config/routes.rb
+++ MyEventApp/config/routes.rb
@@ -27,13 +27,10 @@ MyEventApp::Application.routes.draw do
   get 'calendar/events' => 'calendar#events'
   get 'calendar/event_details' => 'calendar#event_details'
   get 'calendar/accept' => 'calendar#accept'
-<<<<<<< HEAD
   get 'calendar/reject' => 'calendar#reject'
   get 'calendar/maybe' => 'calendar#maybe'
-=======
   get 'calendar/feed/:id/feed' => 'calendar#feed', :as => 'cal_feed_path'
   
->>>>>>> origin/travis_branch
   get 'invitations/show' => 'invitations#show'
   get 'invitations/accept' => 'invitations#accept'
 
